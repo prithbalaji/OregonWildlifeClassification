f5f18c187076a31b45f0ea51f180fa416c0a4cbc
SimpleGeometry <1colin.lu@gmail.com>
2020-10-19 14:42:27 -0700

    Add basic navigation functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, NavDropdown, Button, Form, FormControl } from 'react-bootstrap';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './components/Home';
+import About from './components/About';
+import Error from './components/Error';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <div>
+          <Navigation />
+            <Switch>
+             <Route path="/" component={Home} exact/>
+             <Route path="/about" component={About}/>
+            <Route component={Error}/>
+           </Switch>
+        </div> 
+      </BrowserRouter>
   );
 }
 
