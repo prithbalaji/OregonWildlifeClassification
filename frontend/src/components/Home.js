7a388fc6986624212cb57c31861fef998695f4b8
SimpleGeometry <1colin.lu@gmail.com>
2020-10-19 15:59:05 -0700

    Add initial structure to dropzone feature

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
- 
+import DropZone from './dropzone/DropZone';
+
 function Home() {
     return (
-      <Jumbotron fluid>
-          <Container>
-            <h1>Team 10 Landing Page</h1>
-            <p>
-              You know we kick em out if they ain't with the crew, yeah im talkin about you you begging for attention talking shit on twitter too
-            </p>
-          </Container>
+		<Jumbotron fluid>
+        	<Container>
+            	<h1>Team 10 Landing Page</h1>
+            	<p>
+              		You know we kick em out if they ain't with the crew, yeah im talkin about you you begging for attention talking shit on twitter too
+            	</p>
+
+				<div>
+		        	<p className="title">React Drag and Drop Image Upload</p>
+		        	<div className="content">
+		            	<DropZone />
+		        	</div>
+		    	</div>
+			</Container>
         </Jumbotron>
     );
 }
