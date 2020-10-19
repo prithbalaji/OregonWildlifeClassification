b3a9d27111ccb453a65209bd39978bc0e864c5b1
Carlos Conley <carlosconley@icloud.com>
2020-10-19 18:49:45 -0500

    Add colors to theme

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
- 
+import Button from 'react-bootstrap/Button';
+
 function Home() {
     return (
       <Jumbotron fluid>
@@ -10,10 +11,13 @@ function Home() {
             <p>
               You know we kick em out if they ain't with the crew, yeah im talkin about you you begging for attention talking shit on twitter too
             </p>
+            <p>
+                <Button variant="primary">Learn more</Button>
+            </p>
           </Container>
         </Jumbotron>
     );
 }
- 
+
 export default Home;
 
--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -5,7 +5,7 @@ import { Navbar, Nav } from 'react-bootstrap';
 function Navigation() {
 	return (
 		<Navbar bg="dark" variant="dark">
-			<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+			<Navbar.Brand href="/">Navbar</Navbar.Brand>
 			<Nav className="mr-auto">
 				<Nav.Link href="/">Home</Nav.Link>
 				<Nav.Link href="/about">About</Nav.Link>
