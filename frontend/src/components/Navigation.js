fdf1bde1e9581a41791cd8106e4492279db3943e
Carlos Conley <carlosconley@icloud.com>
2020-10-21 15:31:31 -0500

    Add elements for data viz to navbar

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 function Navigation() {
 	return (
 		<Navbar bg="dark" variant="dark">
-			<Navbar.Brand href="/">Navbar</Navbar.Brand>
+			<Navbar.Brand href="/" className="text-primary">WildCoders</Navbar.Brand>
 			<Nav className="mr-auto">
-				<Nav.Link href="/">Home</Nav.Link>
+				<Nav.Link href="/Construction">Model</Nav.Link>
+				<NavDropdown title="Data Visulization" id="collasible-nav-dropdown">
+					<NavDropdown.Item href="/Construction">Prediction Probability</NavDropdown.Item>
+					<NavDropdown.Divider />
+					<NavDropdown.Item href="/Construction">Training Visualiation</NavDropdown.Item>
+					<NavDropdown.Divider />
+					<NavDropdown.Item href="/Construction">Dataset Imbedding</NavDropdown.Item>
+					<NavDropdown.Divider />
+					<NavDropdown.Item href="/Construction">Activation Map</NavDropdown.Item>
+				</NavDropdown>
 				<Nav.Link href="/about">About</Nav.Link>
 		    </Nav>
 		</Navbar>
