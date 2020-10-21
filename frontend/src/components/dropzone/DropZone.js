f7f386f29aebd3c07d14f46abffe3c9455fb4bf9
SimpleGeometry <1colin.lu@gmail.com>
2020-10-21 14:10:29 -0700

    Add upload file and display image functionality

--- frontend/src/components/dropzone/DropZone.js
+++ frontend/src/components/dropzone/DropZone.js
@@ -1,39 +1,96 @@
 import React from 'react';
-import './DropZone.css'
+import './DropZone.css';
 
-function DropZone() {
-	const dragOver = (e) => {
-		e.preventDefault();
-	}
-
-	const dragEnter = (e) => {
-		e.preventDefault();
-	}
-
-	const dragLeave = (e) => {
-		e.preventDefault();
-	}
-
-	const fileDrop = (e) => {
-		e.preventDefault();
-		const files = e.dataTransfer.files;
-		console.log(files);
+// Cited from:
+// https://codepen.io/hartzis/pen/VvNGZP?editors=0010
+// https://blog.logrocket.com/create-a-drag-and-drop-component-with-react-dropzone/
+class DropZone extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {file: '', imagePreviewUrl: ''};
 	}
 	
-	return (
-		<div className="container">
-			<div className="drop-container"
-				onDragOver={dragOver}
-				onDragEnter={dragEnter}
-				onDragLeave={dragLeave}
-				onDrop={fileDrop}
-			>
+	handleImageChange(e) {
+		e.preventDefault();
+
+		let reader = new FileReader();
+		let file = e.target.files[0];
+
+		reader.onloadend = () => {
+			this.setState({
+			    file: file,
+			    imagePreviewUrl: reader.result
+		    });
+		}
+		
+		if (file) {
+			reader.readAsDataURL(file);
+		}
+	}
+
+	dragOver(e) {
+		e.preventDefault();
+	}
+
+	dragEnter(e) {
+		e.preventDefault();
+	}
+
+	dragLeave(e) {
+		e.preventDefault();
+	}
+
+	fileDrop(e) {
+		e.preventDefault();
+	
+		let reader = new FileReader();
+		let file = e.dataTransfer.files[0];
+		
+		console.log(file)
+
+		reader.onloadend = () => {
+			this.setState({
+			    file: file,
+			    imagePreviewUrl: reader.result
+		    });
+		}
+		
+		if (file) {
+			reader.readAsDataURL(file);
+		}
+	}
+	
+	render() {
+		let {imagePreviewUrl} = this.state;
+		let imagePreview = null;
+		if (imagePreviewUrl) {
+			imagePreview = (<img src={imagePreviewUrl} />);
+		} else {
+			imagePreview = (
 				<div className="drop-message">
 					<div className="upload-icon"></div>
 					Drag & drop files here or click to upload
 				</div>
+			);
+		}
+
+		return (
+			<div className="container">
+				<div className="drop-container"
+					onDragOver={(e) => this.dragOver(e)}
+					onDragEnter={(e) => this.dragEnter(e)}
+					onDragLeave={(e) => this.dragLeave(e)}
+					onDrop={(e) => this.fileDrop(e)}
+				>
+					{imagePreview}
+				</div>
+				<form>
+          			<input className="fileInput" 
+            			type="file" 
+            			onChange={(e) => this.handleImageChange(e)} />
+        		</form>
 			</div>
-		</div>
-	)
+		);
+	}
 }
 export default DropZone;
