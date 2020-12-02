90efb08d05409c1990577e71e38b21000427bfed
SimpleGeometry <1colin.lu@gmail.com>
2020-12-02 15:13:10 -0800

    Change views.py in server to retrieve model from backend folder

--- server/wildlife/views.py
+++ server/wildlife/views.py
@@ -6,7 +6,7 @@ import torchvision.transforms as transforms
 
 # To allow importing model when the server is run from the server folder
 import sys, os
-sys.path.append(os.path.join(os.getcwd(), 'wildlife'))
+sys.path.append(os.path.join(os.getcwd(), '..', 'backend/wildlifeTrainer'))
 
 import model
 
@@ -17,15 +17,15 @@ def index(request):
         # Write the binary string representing the image to a file,
         # and use PIL's from-file image loader to make the PIL image "img" variable.
         # Transform to a tensor to pass into the model.
-        with open("test", "wb") as f:
+        with open("image_temp", "wb") as f:
             f.write(request.body)
-        
-        with open("test", "rb") as f:
+        with open("image_temp", "rb") as f:
             img = Image.open(f)
             image_tensor = transforms.ToTensor()(img)
+        os.remove("image_temp")
 
         # Loading the trained model
-        PATH = 'wildlife/wild_net.pth'
+        PATH = os.path.join(os.getcwd(), '..', 'backend/wildlifeTrainer/wild_net.pth')
         net = model.Net()
         net.load_state_dict(torch.load(PATH))
 
