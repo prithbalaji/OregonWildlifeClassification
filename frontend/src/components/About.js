76c8bca149b56929dd56eb2a28eebfccb4ef677f
Carlos Conley <carlosconley@icloud.com>
2020-12-02 18:43:37 -0600

    Fix about page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -11,54 +11,52 @@ function About() {
 					<p className="text-warning text-left">
 						Hello, we are the WildCoders and this is our group project for CS196 at UIUC. We hope you look around and find something interesting!
 					</p>
-					<hr/>
 				</Container>
-				<Container fluid>
-					<Row style={{ height: '22em' }}>
-						<Col>
+			</Jumbotron>
+				<Container>
+					<Row style={{ height: '25em' }}>
+						<Col className="row justify-content-center">
 						<AboutCard
 							title="Carlos Conley"
 							text="Frontend"
 							github="https://github.com/carlosconley"/>
 						</Col>
-						<Col>
+						<Col className="row justify-content-center">
 						<AboutCard
 							title="Colin Lu"
 							text="Frontend"
 							github="https://github.com/SimpleGeometry"/>
 						</Col>
-						<Col>
+						<Col className="row justify-content-center">
 							<AboutCard
 								title="David Valencia"
 								text="Backend"
-								image=""
 								github="https://github.com/" />
 						</Col>
 					</Row>
-					<Row style={{ height: "22em" }}>
-						<Col>
+					<Row style={{ height: "25em" }}>
+						<Col className="row justify-content-center">
 						<AboutCard
 							title="Carl Chen"
 							text="Backend"
 							github="https://github.com/"/>
 						</Col>
-						<Col>
+						<Col className="row justify-content-center">
 						<AboutCard
 							title="Prithvi Balaji"
 							text="Backend"
 							github="https://github.com/"/>
 						</Col>
-						<Col>
+						<Col className="row justify-content-center">
 						<AboutCard
 							title="Jiwung Cha"
 							text="Modeling"
 							github="https://github.com/"/>
 						</Col>
 					</Row>
-					<Row style={{ height: "22em" }}>
-
+					<Row style={{ height: "25em" }}>
 						<Col></Col>
-						<Col>
+						<Col className="row justify-content-center">
 						<AboutCard
 							title="Ahmed Hasan"
 							text="PM"
@@ -67,7 +65,6 @@ function About() {
 						<Col></Col>
 					</Row>
 				</Container>
-			</Jumbotron>
 		</div>
 	);
 }
