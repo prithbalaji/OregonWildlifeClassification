65c5fccdb62e00374544d89f22f8faba29fbf7b6
SimpleGeometry <1colin.lu@gmail.com>
2020-12-02 19:43:13 -0800

    Refactor model and testModel, and move softmax from NN to server

--- backend/wildlifeTrainer/model.py
+++ backend/wildlifeTrainer/model.py
@@ -17,6 +17,8 @@ def imshow(img):
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
+# The network outputs logits, and softmax should be applied
+# to retrieve the probabilities.
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -33,11 +35,12 @@ class Net(nn.Module):
         x = x.view(-1, 16 * 21 * 21)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.softmax(self.fc3(x), dim=1)
+        x = self.fc3(x)
         return x    
 
 def train_model(net, optimizer, criterion, epochs):
-    minibatches_per_print = 2
+    minibatches_per_interval = 100
+    net.load_state_dict(torch.load('.wild_net.pth'))
 
      # Loop over the dataset multiple times
     for epoch in range(epochs):
@@ -45,7 +48,6 @@ def train_model(net, optimizer, criterion, epochs):
         for i, data in enumerate(trainloader, 0):
             # Get the inputs; data is a list of [inputs, labels]
             inputs, labels = data
-            print(type(inputs))
 
             # Zero the parameter gradients
             optimizer.zero_grad()
@@ -58,19 +60,31 @@ def train_model(net, optimizer, criterion, epochs):
 
             running_loss += loss.item()
 
-            # Print training statistics
-            if i % minibatches_per_print == minibatches_per_print - 1:    # print every minibatches_per_print mini-batches
-                print('[Model %d, mini-batch %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / minibatches_per_print))
+            # Print training statistics and save model checkpoint every minibatches_per_interval loops
+            if i % minibatches_per_interval == minibatches_per_interval - 1: 
+                print('[Epoch %d, mini-batch %5d] loss: %.3f' %
+                    (epoch + 1, i + 1, running_loss / minibatches_per_interval))
                 running_loss = 0.0
+                torch.save(net.state_dict(), '.wild_net.pth')
 
     print('Finished Training')
 
 def getClassNames():
     return ['bald_eagle', 'black_bear', 'bobcat', 'canada_lynx', 'columbian_black-tailed_deer', 
-    'cougar', 'coyote', 'deer', 'elk', 'gray_fox', 'gray_wolf', 'mountain_beaver', 'nutria', 'raccoon',
-    'raven', 'red_fox', 'ringtail', 'sea_lions', 'seals', 'virginia_opossum']
+        'cougar', 'coyote', 'deer', 'elk', 'gray_fox', 'gray_wolf', 'mountain_beaver', 'nutria', 'raccoon',
+        'raven', 'red_fox', 'ringtail', 'sea_lions', 'seals', 'virginia_opossum']
 
+def getTrainAndTestLoaders():
+    data_path = './data/archive/oregon_wildlife/oregon_wildlife'
+
+    transform = transforms.Compose([transforms.Resize(size=(96,96)) ,transforms.ToTensor()])
+
+    trainset = torchvision.datasets.ImageFolder(root=data_path, transform=transform)
+    trainloader = data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+    
+    testset = torchvision.datasets.ImageFolder(root=data_path, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+    return trainloader, testloader
 
 def trainSave():
     train_model(net, optimizer, criterion, 4)
@@ -78,16 +92,8 @@ def trainSave():
     torch.save(net.state_dict(), PATH)
 
 if __name__ == '__main__':
-    data_path = './data/archive/oregon_wildlife/oregon_wildlife'
-
-    transform = transforms.Compose([transforms.Resize(size=(96,96)) ,transforms.ToTensor()])
-
-    trainset = torchvision.datasets.ImageFolder(root=data_path, transform=transform)
-    trainloader = data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
-
-    testset = torchvision.datasets.ImageFolder(root=data_path, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
-
+    trainloader, testloader = getTrainAndTestLoaders()
+    
     if torch.cuda.is_available():
         print("CUDA is available. Attempting to set device to GPU.")
         device = torch.cuda.device("cuda:0")
@@ -98,16 +104,18 @@ if __name__ == '__main__':
     # Mapping each index to a class name
     classes = getClassNames()
 
+    '''
     # Get some random training images to display (verifying the images in the dataset are what we expect)
     dataiter = iter(trainloader)
     images, labels = dataiter.next()
     imshow(torchvision.utils.make_grid(images))
     # Print labels for the images we're displaying
     print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+    '''
 
     # Define the network, loss function, and optimizer
     net = Net()
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
 
     trainSave()
--- backend/wildlifeTrainer/testModel.py
+++ backend/wildlifeTrainer/testModel.py
@@ -12,23 +12,26 @@ import torch.cuda
 import model
 
 def testModel():
-    PATH = './wildlifeTrainer/wild_net.pth' 
-    dataiter = iter(model.testloader)
+    # Load the data
+    _, testloader = model.getTrainAndTestLoaders()
+    dataiter = iter(testloader)
     images, labels = dataiter.next()
+    
     # Load our trained model
+    PATH = 'wild_net.pth'
     net = model.Net()
     net.load_state_dict(torch.load(PATH))
     outputs = net(images)
     print(outputs)
     _, predicted = torch.max(outputs, 1)
-    print('Predicted: ', ' '.join('%5s' % model.classes[predicted[j]]
+    print('Predicted: ', ' '.join('%5s' % model.getClassNames()[predicted[j]]
                               for j in range(4)))
 
     # Let's see how it tests on the whole data set
     correct = 0
     total = 0
     with torch.no_grad():
-        for data in model.testloader:
+        for data in testloader:
             images, labels = data
             outputs = net(images)
             _, predicted = torch.max(outputs.data, 1)
